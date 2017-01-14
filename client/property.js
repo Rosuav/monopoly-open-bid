122b271049ffc76fce9be83e93951c98a1b000df
Chris Angelico <rosuav@gmail.com>
2017-01-15 05:22:48 +1100

    Tidy up the bidding when you're outbid

--- client/property.js
+++ client/property.js
@@ -10,7 +10,8 @@ export default connect()(class Property extends React.Component {
 
 	render() {
 		let minbid = this.props.bidder ? ((this.props.highbid|0) + 10) : (this.props.facevalue|0);
-		if (this.refs.bid && (this.refs.bid.value|0) < minbid) this.refs.bid.value = minbid;
+		//If you've been outbid, blank the field.
+		if (this.refs.bid && (this.refs.bid.value|0) < minbid) this.refs.bid.value = "";
 		return <form onSubmit={this.update.bind(this)}>
 			<h3 style={{backgroundColor: this.props.color, color: this.props.fg || "black"}}>{this.props.name}</h3>
 			<p>
@@ -18,9 +19,10 @@ export default connect()(class Property extends React.Component {
 				{this.props.bidder || "(nobody)"} {this.props.highbid || this.props.facevalue}
 			</p>
 			<div>
-				<input type="number" ref="bid" defaultValue={minbid} />
+				<input type="number" ref="bid" defaultValue={this.props.bidder ? "" : this.props.facevalue} />
 				<input type="submit" value="Bid" />
 			</div>
+			<div>{this.props.bidder ? <input type="submit" value={"Bid " + minbid} /> : "\xa0"}</div>
 		</form>;
 	}
 })
