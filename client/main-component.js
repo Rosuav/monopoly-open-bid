f849ef6918342ff2e31c9484da47a0a452ac9f19
Chris Angelico <rosuav@gmail.com>
2017-01-15 06:02:05 +1100

    Tidy up the funds display a bit

--- client/main-component.js
+++ client/main-component.js
@@ -8,7 +8,7 @@ class MainComponent extends React.Component {
 		return <div>
 			<h1>Monopoly Open Bidding</h1>
 			<p>Place bids on property, yada yada</p>
-			<ul className="userlist">{this.props.users.map((u,i) => <li key={i}>{u[0] + ": " + u[1]}</li>)}</ul>
+			<ul className="userlist"><li>Funds:</li>{this.props.users.map((u,i) => <li key={i}>{u[0] + ": " + u[1]}</li>)}</ul>
 			{this.props.user ? this.props.order.map((p,i) => <Property key={i} name={p} {...this.props.properties[p]}/>) : <Login/>}
 		</div>;
 	}
