ee09a28b2c817f835988f6860e87bba9677c6fbc
Chris Angelico <rosuav@gmail.com>
2017-01-20 23:48:12 +1100

    Add a TODO

--- client/websocket.js
+++ client/websocket.js
@@ -1,5 +1,7 @@
 import store from './store';
 
+//TODO: Establish a connection only when we join a room?
+//Or establish a connection, and then choose a room over it?
 const protocol = window.location.protocol == "https:" ? "wss://" : "ws://";
 const socket = new WebSocket(protocol + window.location.host + "/ws");
 socket.onopen = () => {console.log("Socket connection established.");};
