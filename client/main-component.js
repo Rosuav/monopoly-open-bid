ba21ef056066954da062ff9d24514b63ae24d4a1
Chris Angelico <rosuav@gmail.com>
2022-11-13 05:24:06 +1100

    Transform Done, and we're done!

--- client/main-component.js
+++ client/main-component.js
@@ -2,11 +2,11 @@ import {lindt, DOM, on} from "https://rosuav.github.io/choc/factory.js";
 const {DIV, H1, LI, P, UL} = lindt; //autoimport
 import Property from './property.js';
 import Login from './login.js';
-//import Done from './done';
+import Done from './done.js';
 
 export default function render(state) {
 	return DIV([
-		state.user && !state.all_done && "Done() goes here",
+		state.user && !state.all_done && Done(state),
 		H1("Monopoly Open Bidding"),
 		P("Place bids on property, yada yada"),
 		UL({class: "userlist"}, [
