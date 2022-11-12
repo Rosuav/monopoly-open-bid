98e75c13dccf75e7b89164f0b1d47afd56cbe779
Chris Angelico <rosuav@gmail.com>
2022-11-13 04:53:18 +1100

    Transform the login component

--- client/main-component.js
+++ client/main-component.js
@@ -1,7 +1,7 @@
 import {lindt, DOM, on} from "https://rosuav.github.io/choc/factory.js";
 const {DIV, H1, LI, P, UL} = lindt; //autoimport
 //import Property from './property';
-//import Login from './login';
+import Login from './login.js';
 //import Done from './done';
 
 export default function render(state) {
@@ -12,7 +12,7 @@ export default function render(state) {
 		UL({class: "userlist"}, [
 			LI("Funds:"),
 			state.users.map(u => LI(u[0] + ": " + u[1])),
-			state.user ? state.order.map(p => "Property(state, p, state.props.properties[p])") : "Login()",
+			state.user ? state.order.map(p => "Property(state, p, state.props.properties[p])") : Login(state),
 		]),
 	]);
 }
